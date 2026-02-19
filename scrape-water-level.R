5df090412a7a9dae23822ea23425c63df5560573
Jakob Listabarth <76778035+jakoblistabarth@users.noreply.github.com>
2026-02-19 10:57:29 +0100

    :alembic: Sort parquet file, use antijoin for merging

--- scrape-water-level.R
+++ scrape-water-level.R
@@ -5,23 +5,23 @@ library(nanoparquet)
 url <- "https://www.hnd.bayern.de/pegel/iller_lech/lindau-20001001/tabelle?methode=seewasserstand&begin=01.01.2023&setdiskr=15"
 lake_constance_zero <- 391.84
 
-
 # read in old -------------------------------------------------------------
 
-old_levels <- read_parquet("water_level.parquet") |> 
-  mutate(date = as.POSIXct(date, format = "%d.%m.%Y %H:%M"))
+old_levels <- read_parquet("water_level.parquet") |>
+  mutate(date = as.POSIXct(date, format = "%d.%m.%Y %H:%M")) |>
+  arrange(date)
 
 # read in new -------------------------------------------------------------
 
-new_levels <- read_html(url) |> 
+new_levels <- read_html(url) |>
   html_table(dec = ",") %>%
-  `[[`(1) |> 
-  setNames(c("date", "water_level_m_nhn")) |> 
-  mutate(date = as.POSIXct(date, format = "%d.%m.%Y %H:%M")) |> 
-  filter(!is.na(date)) |> 
+  `[[`(1) |>
+  setNames(c("date", "water_level_m_nhn")) |>
+  mutate(date = as.POSIXct(date, format = "%d.%m.%Y %H:%M")) |>
+  filter(!is.na(date)) |>
   summarize(water_level_m_nhn = mean(water_level_m_nhn),
-            .by = date) |> 
-  mutate(water_level_cm_lake = (water_level_m_nhn - lake_constance_zero) * 100) |> 
+            .by = date) |>
+  mutate(water_level_cm_lake = (water_level_m_nhn - lake_constance_zero) * 100) |>
   mutate(water_level_cm_lake = as.integer(water_level_cm_lake))
 
 print("Start:")
@@ -31,11 +31,12 @@ print(new_levels |> tail(2))
 
 # merge -------------------------------------------------------------------
 
-levels <- old_levels |> 
-  bind_rows(new_levels) |> 
-  distinct()
+levels <- old_levels |>
+  bind_rows(
+    anti_join(new_levels |> arrange(date), old_levels)
+  )
 
-unique_dates <- levels$date |> unique() |> length() 
+unique_dates <- levels$date |> unique() |> length()
 rows_levels <- nrow(levels)
 if(unique_dates != rows_levels){
   message("Rows old levels:", nrow(old_levels), "\nRows new:", nrow(new_levels))
